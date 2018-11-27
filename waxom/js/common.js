af987f63d167fa277e9c12fd5361e82b69637028
kovalchukpvl <45391586+kovalchukpvl@users.noreply.github.com>
2018-11-27 16:34:37 +0200

    Add files via upload

---
+++ waxom/js/common.js
@@ -0,0 +1,59 @@
+$('.wallper').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 1000,
+  dots: true,
+  easing: "ease",
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        dots: true
+      }
+    }
+  ]
+});
+
+$('.recent-posts-container').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
+
+  function myFunction() {
+    var checkBox = document.getElementById("myCheck");
+    var niceCheckbox = document.getElementById("niceCheckbox");
+    var headermenu = document.getElementById("headermenu");
+
+    if (checkBox.checked == true){
+      niceCheckbox.style.backgroundImage = "url(../img/menu-mobile-hover.png)";
+      headermenu.style.display = "grid";
+    } else {
+      niceCheckbox.style.backgroundImage = "url(../img/menu-mobile.png)";
+      headermenu.style.display = "none";
+    }
+
+  }
+
+
+
